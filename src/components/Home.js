65927a66dccf84ff656e16617dd337fd6df8ef55
Niks156 <nayangarg408@gmail.com>
2022-05-30 15:55:25 +0530

    updated

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,28 +1,10 @@
-import React, { useEffect, useState } from "react";
+import React from "react";
 import { useAuth } from "../context/Userauthcontext";
 import Box from "@mui/material/Box";
-import { getAuth, updateProfile } from "firebase/auth";
 import { Button } from "@mui/material";
 
 export default function Home() {
   const { user, logout } = useAuth();
-  const [name, setName] = useState("Niks garg");
-  const [photourl, setphotourl] = useState(
-    "https://media.istockphoto.com/photos/global-connection-picture-id1335295813?k=20&m=1335295813&s=612x612&w=0&h=99MXI041vwF4Ji0jD5Ni8AEJIDxxUZAvy0HPwWWTre8="
-  );
-
-  {
-    updateProfile(user, {
-      displayName: name,
-      photoURL: photourl,
-    })
-      .then(() => {
-        console.log("updated");
-      })
-      .catch((error) => {
-        console.log("error occured");
-      });
-  }
 
   const handlesubmit = async () => {
     try {
